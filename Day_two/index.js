58dc36bd92041b4b12d30d6c3825ebc62d494a82
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-04-16 21:16:24 +0200

    Add returned reversed array with condition

--- Day_two/index.js
+++ Day_two/index.js
@@ -1,6 +1,17 @@
 function solve(arr){
     let reversedArr = arr.reverse();
-  reversedArr
+
+  return reversedArr.map((cur,index,target)=>{
+    if(index === 0) {
+        return cur.replace(/(Left|Right)/,'Begin');
+    }
+    if (target[index - 1].includes('Left')) {
+        return cur.replace(/Left|Right|Begin/,'Right')
+    }
+    if (target[index - 1].includes('Right')) {
+        return cur.replace(/Left|Begin|Right/,'Left');
+    }
+  })
 }
 
-solve(["Begin on Road A","Right on Road B","Right on Road C","Left on Road D"])
+console.log(solve(["Begin on Road A","Right on Road B","Right on Road C","Left on Road D"]))
