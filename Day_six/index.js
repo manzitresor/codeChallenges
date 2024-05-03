9b19affeadddf5e485e368da3d79aca8f24e5250
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-05-03 11:04:45 +0200

    Add checked if number is prime or not

--- Day_six/index.js
+++ Day_six/index.js
@@ -1,5 +1,23 @@
-  const sexyPrime = (x, y) => [x,y].sort((a,b) => a - b).reduce((acc,curr) => acc = curr - acc,0) === 6;
+
+function primeChecker(a) {
+   if( a <= 1) return false;
+   if( a === 2) return true;
+   const maxDivisor = Math.sqrt(a);
+   for(let i = 2; i <= maxDivisor;i++) {
+    if( a % i === 0) {
+      return false
+    }
+   }
+   return true
+}
+
+
+function sexyPrime(x, y){
+    let result = [x,y].sort((a,b) => a - b).reduce((acc,curr) => acc = curr - acc,0);
+    return result === 6 && (primeChecker(x)&&primeChecker(y));
+  }
+
   console.log(sexyPrime(5, 11)) //-->  true
   console.log(sexyPrime(7, 13)) //-->  true
   console.log(sexyPrime(5, 7)) //-->  false
-
+  console.log(sexyPrime(1, 7)) //-->  false
